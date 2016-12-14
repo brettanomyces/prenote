1dd4f38182480765199914d27636936fb3ad4bd6
vagrant <vagrant@homestead>
2016-12-14 06:11:09 +0000

    create schema

---
+++ database/migrations/2016_12_14_054316_create_tickets_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateTicketsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('tickets', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('title');
+            $table->text('description');
+            $table->string('priority');
+            $table->integer('customer_id')->unsigned();
+            $table->timestamps();
+        });
+
+        Schema::table('tickets', function(Blueprint $table) {
+            $table->foreign('customer_id')->references('id')->on('customers');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('tickets');
+    }
+}
